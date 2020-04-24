34f39b6c967602dac9589463272215f8074ffecc
David Podroužek <david.podrouzek@juicymo.cz>
2020-04-24 12:56:33 +0200

    Add version with chromium bundled

--- build.sh
+++ build.sh
@@ -28,28 +28,40 @@ fi
 ${sudo_string} docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
 
 build_version (){
-    image_version=$1
-    pg_version=$2
-    sudo_string=$3
-    general="$(echo ${image_version} | cut -d '|' -f1 -s)"
-    image_version="$(echo ${image_version} | cut -d '|' -f2)"
-    ruby_version="$(echo ${image_version} | cut -d '-' -f1)"
+  image_version=$1
+  pg_version=$2
+  sudo_string=$3
+  general="$(echo ${image_version} | cut -d '|' -f1 -s)"
+  image_version="$(echo ${image_version} | cut -d '|' -f2)"
+  ruby_version="$(echo ${image_version} | cut -d '-' -f1)"
 
-    sed -e "s/%%FROM%%/$image_version/g" \
-        -e "s/%%TAG%%/$ruby_version/g" \
-        -e "s/%%POSTGRES%%/$pg_version/g" \
-     Dockerfile.template > Dockerfile
+  sed -e "s/%%FROM%%/$image_version/g" \
+      -e "s/%%TAG%%/$ruby_version/g" \
+      -e "s/%%POSTGRES%%/$pg_version/g" \
+      Dockerfile.template > Dockerfile
 
-     ${sudo_string} docker build .
-     ${sudo_string} docker build -t juicymo/drone-ruby:${ruby_version} .
-     ${sudo_string} docker push juicymo/drone-ruby:${ruby_version}
+  ${sudo_string} docker build .
+  ${sudo_string} docker build -t juicymo/drone-ruby:${ruby_version} .
+  ${sudo_string} docker push juicymo/drone-ruby:${ruby_version}
 
-    if [[ ${general} == "g" ]]
-    then
-        minor_version="$(echo ${ruby_version} | cut -d '.' -f1-2)"
-        ${sudo_string} docker build -t juicymo/drone-ruby:${minor_version} .
-        ${sudo_string} docker push juicymo/drone-ruby:${minor_version}
-    fi
+  if [[ ${general} == "g" ]]
+  then
+      minor_version="$(echo ${ruby_version} | cut -d '.' -f1-2)"
+      ${sudo_string} docker build -t juicymo/drone-ruby:${minor_version} .
+      ${sudo_string} docker push juicymo/drone-ruby:${minor_version}
+  fi
+
+  echo "RUN apk add --no-cache chromium" >> Dockerfile
+  ${sudo_string} docker build .
+  ${sudo_string} docker build -t juicymo/drone-ruby:${ruby_version}-chrome .
+  ${sudo_string} docker push juicymo/drone-ruby:${ruby_version}-chrome
+
+  if [[ ${general} == "g" ]]
+  then
+      minor_version="$(echo ${ruby_version} | cut -d '.' -f1-2)"
+      ${sudo_string} docker build -t juicymo/drone-ruby:${minor_version}-chrome .
+      ${sudo_string} docker push juicymo/drone-ruby:${minor_version}-chrome
+  fi
 }
 
 if [[ ${image} == "" ]] ; then
